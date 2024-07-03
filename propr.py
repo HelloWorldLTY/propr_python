396d1a40d60e88c44085feb12c8cca5f01988dc1
HelloWorldLTY <tianyu.18@intl.zju.edu.cn>
2024-07-02 19:10:41 -0700

    first commit

---
+++ propr.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.stats import gmean
+
+def clr(a1):
+    gm =  gmean(a1)
+    return np.log(a1 / gm + 1e-8)
+
+def alr(a1):
+    x_d = a1[-1]
+    return np.log(a1[0:len(a1)-1] / x_d + 1e-8)
+
+def phi(ai,aj):
+    return np.var(ai-aj) / np.var(ai)
+
+def propr_corr(ai,aj):
+    return 1 - np.var(ai-aj) / (np.var(ai) + np.var(aj))
+
+def phi_s(ai,aj):
+    return np.var(ai-aj) / np.var(ai+aj)
+
+
+def test_propr():
+    a1 = np.array([1,2,3,4,5])
+    a2 = np.array([1,2,3,4,1])
+
+    p1 = propr_corr(clr(a1), clr(a2))
+    p2 = phi(clr(a1), clr(a2))
+    p3 = phi_s(clr(a1), clr(a2))
+
+    p4 = propr_corr(alr(a1), alr(a2))
+    p5 = phi(alr(a1), alr(a2))
+    p6 = phi_s(alr(a1), alr(a2))
+
+    print(p1,p2,p3,p4,p5,p6)
+
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Python version of correlation method: propr
+
+This is the implementation of a method to compute correlation: propr.
+
+The R reference can be found in: https://github.com/tpq/propr
+
+The paper reference can be found in: https://www.nature.com/articles/s41598-017-16520-0
